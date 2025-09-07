3860abde19e50675facf2c01a7d8f07c43dd4156
Kim Enorme <kimenorme15@gmail.com>
2025-09-07 12:12:16 +0800

    Task 1

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,27 +1,48 @@
+import PropTypes from 'prop-types';
+import styles from './ProfileStyle.module.css';
+
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
+      <ul className={styles.stats}>
+        <li className={styles.statsList}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>
+            {stats.followers.toLocaleString()}
+          </span>
         </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
+        <li className={styles.statsList}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>
+            {stats.views.toLocaleString()}
+          </span>
         </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.liker}</span>
+        <li className={styles.statsList}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>
+            {stats.likes.toLocaleString()}
+          </span>
         </li>
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
